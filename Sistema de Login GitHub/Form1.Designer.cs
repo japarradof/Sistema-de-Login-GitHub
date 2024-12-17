b828ae885b90a937719b10957425a8ef2741b675
Juan Antonio <152283035+TheHuanPa@users.noreply.github.com>
2024-12-17 13:00:10 +0100

    segundocommit

--- Sistema de Login GitHub/Form1.Designer.cs
+++ Sistema de Login GitHub/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -78,11 +79,22 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña:";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Minecraft", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sistema de Login";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(685, 382);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -102,6 +114,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
