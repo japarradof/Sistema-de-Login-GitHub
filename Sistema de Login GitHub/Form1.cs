bd5cd6b7e132c9fcf79eb8222914c80eaa21ba07
Juan Antonio <152283035+TheHuanPa@users.noreply.github.com>
2024-12-17 12:46:22 +0100

    1erCommit

--- Sistema de Login GitHub/Form1.cs
+++ Sistema de Login GitHub/Form1.cs
@@ -16,5 +16,10 @@ namespace Sistema_de_Login_GitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
